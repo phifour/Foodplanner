3ca5fd8aea2bc05895e8b70c5dccfe3d033f9f0b
Wolfgang Frisch <w.frisch@gmx.at>
2022-03-25 22:09:39 +0100

    API fix5

--- foodplaner/settings.py
+++ foodplaner/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'planner.apps.PlannerConfig',
-    'crispy_forms',
+  #  'crispy_forms',
     'rest_framework'
 ]
 
