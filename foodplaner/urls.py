2f7214a0ea583601d3c6b42fdb51a0e8e6495585
phifour <w.frisch@gmx.at>
2022-06-06 23:09:20 +0200

    Update urls.py

--- foodplaner/urls.py
+++ foodplaner/urls.py
@@ -21,7 +21,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('planner/', include('planner.urls')),
-    path('', include('planner.urls'))
+    path('', include('planner.urls'),name='index')
 ]
 
 
